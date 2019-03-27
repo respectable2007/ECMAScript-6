f673651db033079b06e3557df4cdd72f44caa7bd
respectable2007 <zhanghui8876@163.com>
2019-03-27 15:23:22 +0800

    same object literal attributes

--- src/es6/object.js
+++ src/es6/object.js
@@ -1,4 +1,5 @@
 (() => {
+  "use strict";
   /* 对象字面量语法扩展 */
   let foo = 'bar',
       bar = 'juanjuan',
@@ -12,7 +13,9 @@
       [foo]: 'xiaomaomi',
     //   [foo]//报错，属性表达式与简写表示不能同时出现
     };
-  console.log(obj1.bar);//xiaomaomi，[foo]属性覆盖了bar属性（[foo]和bar同名）
+  /* [foo]和bar重名，ES6不会检查重名属性。
+     当出现重名后，该属性实际值为后排那个属性的值 */
+  console.log(obj1.bar);//xiaomaomi
   obj1.sayHi();//xiaomaomi
   console.log(obj1.foo);//undefined
 })();
