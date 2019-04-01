ef5272676a83fb8e1673540497aa45650e33bc87
respectable2007 <zhanghui8876@163.com>
2019-04-01 14:42:48 +0800

    symbol base

--- src/es6/symbol.js
+++ src/es6/symbol.js
@@ -1,3 +1,27 @@
 (() => {
   "use strict";
+  /* 创建符号值 */
+  let symbol = Symbol('url');
+  console.log(symbol);//Symbol('url')
+
+  /* 符号的类型 */
+  console.log(typeof symbol);//symbol
+
+  /* 共享符号 */
+  let pub = Symbol.for('uid'),
+      obj = {
+        [pub] : 'rrr'
+      };
+  console.log(obj);
+  let pub1 = Symbol.for('uid');
+  /* pub、pub1共享Symbol('uid')符号 */
+  console.log(pub === pub1);//true
+
+  /* 符号的转换 */
+  let pub3 = Symbol('uid1');
+  console.log(pub3);//隐式调用toString方法，输出Symbol('uid1')字符串
+  // console.log(pub3 + 1);//报错，符号不能转为number
+  console.log(pub3 && {});//{}，符号值在逻辑运算中认为是true，返回{}对象
+
+  /*  */
 })()
