9a9ef15994c4cbf18bb1f206585f9b08730b149d
respectable2007 <zhanghui8876@163.com>
2019-04-18 11:24:50 +0800

    Promise.resolve

--- src/es6/promise.js
+++ src/es6/promise.js
@@ -3,4 +3,20 @@
   let promise = new Promise((resolved, rejected) => {
     console.log(resolved);
   });
+  /* promie.resolve */
+  /* setTimout会在下次事件循环时，被执行 */
+  setTimeout(()=>{
+    console.log(3);
+  },0)
+  /* Promise.resolve会在本次事件循环结束时，被执行 */
+  new Promise((resolve) => {
+    resolve()
+  }).then(()=>{
+    console.log(2.5);
+  })
+  Promise.resolve().then(()=>{
+    console.log(2);
+  })
+  console.log(1);
+  /* 依次输出 1 2.5 2 3 */
 })()
