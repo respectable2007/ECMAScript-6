079c5eca651f6533e66e0dcc859184decee49789
respectable2007 <zhanghui8876@163.com>
2019-04-18 09:46:27 +0800

    promise

--- nodejs/promise.js
+++ nodejs/promise.js
@@ -1,10 +1,23 @@
 const fs = require('fs');
 function readFile(txt) {
   return new Promise(function(resolve,reject) {
-    console.log(1);
-    fs.readFile(txt);
+    fs.readFile(txt, function(err, data) {
+        if(err) {
+          reject(err);
+          return;
+        }
+        console.log(data);
+        resolve(data);
+    });
   })
 }
 
-let f = readFile('txt/sample.txt');
-console.log(2)
+let f = readFile('../nodejs/txt/sample.txt');
+/* f对应fs.readFile异步操作
+   当读取文件成功时，将then回调函数加入作业队列；
+   读取文件失败时，将catch回调函数加入作业队列 */
+f.then((data) => {
+  console.log(data)
+}).catch((err) => {
+  console.log(err)
+})
