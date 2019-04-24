7c2ed4de57e7b8076d595d360c3b2f6802cef7cc
respectable2007 <zhanghui8876@163.com>
2019-04-24 16:27:43 +0800

    promise native implementation

--- src/es6/promiseInit.js
+++ src/es6/promiseInit.js
@@ -1,105 +1,57 @@
-(()=> {
-  /* 问题2：Promise的底层实现 */
-  function MyPromise(fn) {
-    let missions = [],
-        value = null,
-        fails = [],
-        state = 'Pending';//Promise状态
-    fn(resolve, reject);
-    /* resolve函数 */
-    function resolve(_return_value) {
-      if(state === 'Rejected') {
-        return;
+(() => {
+  function MyPromise (fn) {
+    this.deferreds = [];
+    this.state = 0;
+    this.value = null;
+    let _this = this;//0:pending,1:fulfilled,2:rejected
+    function resolve(return_value) {
+      if(return_value && (typeof return_value === 'object' || typeof return_value === 'function')) {
+        const then = return_value.then;
+        if(typeof then === 'function') {
+          then.call(return_value, resolve);
+          return;
+        }
       }
-      value = _return_value;
-      state = 'Fulfilled';
-      console.log(0);
-      /* 异步操作 */
+      _this.state = 1;
+      _this.value = return_value;
+      /* 若异步操作，跳过异步操作，会先调用then方法（resolve未触发），setTimeout函数保证resolve触发后，
+         then注册函数顺序执行 */
       setTimeout(() => {
-        missions.forEach(mission => {
-          mission(value);
+        _this.deferreds.forEach((deferred) => {
+          deferred(_this.value);
         })
-      },0);
+      }, 0)
     }
-    let next_resolve = null,
-        next_reject = null;
-     /* then方法 */
-    this.then = function(mission) {
-      /* 保存当前的resolve，保证状态正确 */
-      function fn(resolve, reject) {
-        next_resolve = resolve;
-        next_reject = reject;
-        /* if( state === 'Pending') {
-          missions.push(mission);
-        } */
-        if( state === 'Fulfilled') {
-          /* then方法回调函数的返回值 */
-          let result = mission(value);
-          /* 返回thenable对象 */
-          if((Object.prototype.toString.call(result) === '[object Function]' || Object.prototype.toString.call(result) === '[object Object]') && 'then' in result){
-            if(Object.prototype.toString.call(result['then']) === '[object Function]') {
-              result.then(next_resolve);
-            }
-          } else {
-            next_resolve(result);
-          }
-        } else {
-            reject(value);
-        }
-      }
-      /* then方法返回新的Promise对象 */
-      return new MyPromise(fn);
+    fn(resolve);
+  }
+  MyPromise.prototype.then = function(deferred) {
+    if(this.state === 0) {
+      this.deferreds.push(deferred);
+      return;
     }
-    /* reject函数 */
-    function reject(_error) {
-      if(state === 'Fulfilled') {
-        return;
-      }
-      value = _error;
-      state = 'Rejected';
-      setTimeout(() => {
-        fails.forEach(fail => {
-          fail(value);
-        })
-      },0)
+    let _this = this;
+    function handle(resolve) {
+      const ret = deferred(_this.value);
+      /* 上一个then注册函数的返回值，做为其新生成Promise对象的resolve传参 */
+      resolve(ret);
     }
-    /* catch方法 */
-    this.catch = function(fail) {
-      /* 保存当前的resolve，保证状态正确 */
-      function fn(resolve, reject) {
-        next_resolve = resolve;
-        next_reject = reject;
-        if(state === 'Pending') {
-          fails.push(fail);
-        }
-        if(state === 'Rejected') {
-          const result = fail(value);
-          if((Object.prototype.toString.call(result) === '[object Function]' || Object.prototype.toString.call(result) === '[object Object]') && 'then' in result){
-            if(Object.prototype.toString.call(result['then']) === '[object Function]') {
-              result.catch(next_reject);
-            }
-          } else {
-            next_reject(result);
-          }
-        }
-      }
-      return new MyPromise(fn);
-    }
+    return new MyPromise((resolve) => {
+      handle(resolve);
+    })
+    
   }
   let myPromise = new MyPromise((resolve, reject) => {
-    console.log('开始洗衣服。。。');
-    resolve('洗衣服完了');
-    // reject('洗衣机出故障了');
+    /* do async something */
+    resolve('开始洗衣服')
   })
   
-  myPromise.then((data) => { 
+  myPromise.then((data) => {
     console.log(data);
-    console.log('开始晾衣服了');
     return new MyPromise((resolve,reject) => {
-      reject('晾衣杆坏了');
-    });
-  }).catch((e) => {
-    console.log(e);
-    console.log('晾衣杆修好了');
+      resolve('衣服洗完了');
+    })
+  }).then((data) => {
+    console.log(data);
+    return '衣服晾完了'
   })
-})();
+})()
